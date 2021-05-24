fff6fc1cca5c01195f6340cf6dcf1ed031346ff2
Vince Varga <vince.varga@serialdeveloper.com>
2021-05-24 12:17:09 +0200

    Add instruction on how to include rc/main.sh in zshrc

--- rc/main.sh
+++ rc/main.sh
@@ -1,3 +1,11 @@
+# This is the only file to be sourced in your .xyzrc file (.bashrc, .zshrc).
+#
+# Everything else will be added by the source commands below.
+#
+# Assuming the config repository is in ~/.config, you can include this file
+# by executing the following command:
+# $ echo "source ~/.config/rc/main.sh" >> ~/.zshrc
+
 # Source aliases for commonly used programs.
 source ~/.config/rc/aliases/main.sh
 
