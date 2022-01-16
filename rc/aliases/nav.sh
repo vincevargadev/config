7ecad55fa2dad1009b433207502c393fc9fc5ce0
Vince Varga <vince.varga@serialdeveloper.com>
2022-01-16 23:25:31 +0100

    Add cdc alias to change to config dir:

--- rc/aliases/nav.sh
+++ rc/aliases/nav.sh
@@ -5,6 +5,8 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias ......="cd ../../../../.."
 
+alias cdc="cd ~/.config"
+
 # mcd to make a directory and change to that newly created directly in one command
 mcd() {
   mkdir -p -- "$1" && cd -P -- "$1"
