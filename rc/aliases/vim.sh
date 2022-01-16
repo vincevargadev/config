2ea22c73da7993252af37d549ca7957ec3b3a159
Vince Varga <vince.varga@serialdeveloper.com>
2022-01-16 22:33:11 +0100

    Fix vc command to quickly open config folder

--- rc/aliases/vim.sh
+++ rc/aliases/vim.sh
@@ -3,5 +3,8 @@ alias vim=nvim
 alias v=nvim
 
 # TODO: Make sure NERDTree also opens when this command is executed
-alias vc=nvim ~/.config/
+alias vc=_open_config_folder
 
+_open_config_folder() {
+  nvim ~/.config/ +':cd %:h'
+}
