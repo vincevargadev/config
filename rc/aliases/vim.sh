c147ef685cda2b33a8d485dd1cb6f0b1b3a0ab1a
Vince Varga <vince.varga@serialdeveloper.com>
2022-01-20 08:57:58 +0100

    Add alias vz to open .zshrc file
    Close #9

--- rc/aliases/vim.sh
+++ rc/aliases/vim.sh
@@ -4,7 +4,11 @@ alias v=nvim
 
 # TODO: Make sure NERDTree also opens when this command is executed
 alias vc=_open_config_folder
-
 _open_config_folder() {
   nvim ~/.config/ +':cd %:h'
+}
+
+alias vz=_open_zshrc
+_open_zshrc() {
+  nvim ~/.zshrc +':cd %:h'
 }
