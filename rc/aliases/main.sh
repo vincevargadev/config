f8d995bf1e478b571808c48fc2bee7463bc5f1c6
vvarga <vvarga@clickup.com>
2023-08-23 09:50:20 +0200

    Add tmux aliases

--- rc/aliases/main.sh
+++ rc/aliases/main.sh
@@ -5,5 +5,6 @@ source ~/.config/rc/aliases/git.sh
 source ~/.config/rc/aliases/git_aliases.sh
 source ~/.config/rc/aliases/nav.sh
 source ~/.config/rc/aliases/other.sh
+source ~/.config/rc/aliases/tmux.sh
 source ~/.config/rc/aliases/vim.sh
 
---
+++ rc/aliases/tmux.sh
@@ -0,0 +1,12 @@
+# alias k=clear
+# alias q=exit
+# alias ll=ls -lh
+# alias l=ls -lah
+
+alias t=tmux
+
+# _source_rc() {
+  # source ~/.zshrc
+# }
+# alias ss=_source_rc
+
